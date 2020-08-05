989356b34edc2350738889ffdb5066141ba2a6b4
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-05 15:11:49 +0100

    sessionFactory

--- src/main/java/com/fr/gc/utils/HibernateUtil.java
+++ src/main/java/com/fr/gc/utils/HibernateUtil.java
@@ -5,14 +5,14 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-public class HibernateUtil {
+public final class HibernateUtil {
 	private static HibernateUtil instance;
 	private SessionFactory sessionFactory;
 
 	private HibernateUtil() {
 
 		if (sessionFactory == null) {
-			Configuration configuration = new Configuration().configure();
+			Configuration configuration = new Configuration().configure();//hibernate.cfg.xml appl implicite
 			StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
 			serviceRegistryBuilder.applySettings(configuration.getProperties());
 			ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
