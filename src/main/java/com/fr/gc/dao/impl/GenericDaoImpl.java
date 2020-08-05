3f2b28ad2848fd44d36c3bb8d8644f1d50d286c6
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-05 15:11:23 +0100

    Impliment Generic methods

--- src/main/java/com/fr/gc/dao/impl/GenericDaoImpl.java
+++ src/main/java/com/fr/gc/dao/impl/GenericDaoImpl.java
@@ -2,33 +2,59 @@ package com.fr.gc.dao.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Criterion;
 
 import com.fr.gc.dao.GenericDao;
+import com.fr.gc.utils.HibernateUtil;
 
-public class GenericDaoImpl<T,K> implements GenericDao<T, K> {
+public class GenericDaoImpl<T, K> implements GenericDao<T, K> {
+
+	// protected so service wont open session
+	// only for dao
+
+	protected Session hibernateSession;
+	private Transaction tx;
+
+	protected void startOperation() {
+		hibernateSession = HibernateUtil.getInstance().getSessionFactory().openSession();
+		tx = hibernateSession.beginTransaction();
+	}
+
+	protected void endOperation() {
+		tx.commit();
+		hibernateSession.close();
+
+	}
 
 	@Override
 	public void save(T entity) throws Exception {
-		// TODO Auto-generated method stub
-		
+		startOperation();
+		hibernateSession.save(entity); // na7a el insert into balala
+		endOperation();
+
 	}
 
 	@Override
 	public void update(T entity) throws Exception {
-		// TODO Auto-generated method stub
-		
+		startOperation();
+		hibernateSession.update(entity);
+		endOperation();
+
 	}
 
 	@Override
 	public void delete(T entity) throws Exception {
-		// TODO Auto-generated method stub
-		
+		startOperation();
+		hibernateSession.delete(entity);
+		endOperation();
 	}
 
 	@Override
 	public T findById(K id) throws Exception {
-		// TODO Auto-generated method stub
+		startOperation();
+		
 		return null;
 	}
 
