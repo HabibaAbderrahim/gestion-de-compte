427cfaa4284d532f1916b4adaf697ac64b6a6ab2
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-05 13:41:27 +0100

    daoImpli

---
+++ src/main/java/com/fr/gc/dao/impl/CompteDaoImpl.java
@@ -0,0 +1,8 @@
+package com.fr.gc.dao.impl;
+
+import com.fr.gc.dao.CompteDao;
+import com.fr.gc.entities.Compte;
+
+public class CompteDaoImpl extends GenericDaoImpl<Compte, Integer> implements CompteDao{
+
+}
---
+++ src/main/java/com/fr/gc/dao/impl/PersonneDaoImpl.java
@@ -0,0 +1,8 @@
+package com.fr.gc.dao.impl;
+
+import com.fr.gc.dao.PersonneDao;
+import com.fr.gc.entities.Personne;
+
+public class PersonneDaoImpl extends GenericDaoImpl<Personne, String>implements PersonneDao {
+
+}
