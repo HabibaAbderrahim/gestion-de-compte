432bbbcc32d497f44d784c19c8eed98cbdf62141
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-05 15:15:07 +0100

    Test save method

---
+++ src/main/java/com/fr/gc/utils/Test.java
@@ -0,0 +1,37 @@
+package com.fr.gc.utils;
+
+import com.fr.gc.dao.PersonneDao;
+import com.fr.gc.dao.impl.GenericDaoImpl;
+import com.fr.gc.dao.impl.PersonneDaoImpl;
+import com.fr.gc.entities.Personne;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		Personne personne = new Personne("145", "salah", "ahh", "ahha", "hhh");
+		Personne personne1 = new Personne("1475", "salah", "aohh", "ahoha", "hohh");
+		
+		
+		//with couplage
+		PersonneDaoImpl personneDaoImpl = new PersonneDaoImpl() ;
+		try {
+			personneDaoImpl.save(personne);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		//polymo interface for couplage fort/faible
+		PersonneDao personneDao = new PersonneDaoImpl();
+		
+		try {
+			personneDao.save(personne1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
