4b584dfbda3eccc0f96b78b01755c8a7c01334dd
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-05 13:18:18 +0100

    Add Generic Methods

--- src/main/java/com/fr/gc/dao/GenericDao.java
+++ src/main/java/com/fr/gc/dao/GenericDao.java
@@ -1,5 +1,20 @@
 package com.fr.gc.dao;
 
-public interface GenericDao<T,K> {
+import java.util.List;
 
+import org.hibernate.criterion.Criterion;
+
+public interface GenericDao<T, K> {
+
+	public void save(T entity) throws Exception;
+
+	public void update(T entity) throws Exception;
+
+	public void delete(T entity) throws Exception;
+
+	public T findById(K id) throws Exception;
+	
+	public List<T> findAll() throws Exception;
+	
+	public List<T> findByCriteria (Criterion Crit);
 }
