855c847baac94bbc036add9916077bb0c945b4b3
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-08 14:14:22 +0100

    person controller

--- src/main/java/com/fr/gc/beans/PersonneBean.java
+++ src/main/java/com/fr/gc/beans/PersonneBean.java
@@ -5,8 +5,21 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
+import com.fr.gc.entities.Personne;
+
 @ManagedBean
 @ViewScoped
 public class PersonneBean implements Serializable {
 
+	private Personne personne = new Personne();
+
+	public Personne getPersonne() {
+		return personne;
+	}
+
+	public void setPersonne(Personne personne) {
+		this.personne = personne;
+	}
+	
+	
 }
--- src/main/java/com/fr/gc/services/PersonneService.java
+++ src/main/java/com/fr/gc/services/PersonneService.java
@@ -10,7 +10,6 @@ public interface PersonneService {
 	public MessageResponse save(Personne personne) throws Exception;
 	public MessageResponse update(Personne personne) throws Exception;
 	public MessageResponse delete(Personne personne) throws Exception;
-
 	public List<Personne> findAll() throws Exception;
 
 }
