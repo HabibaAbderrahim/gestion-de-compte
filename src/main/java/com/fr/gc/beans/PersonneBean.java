1536bd17dff1e623402eeccd0ace6a6507f34035
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-08 14:13:35 +0100

    Personne Controller

---
+++ src/main/java/com/fr/gc/beans/PersonneBean.java
@@ -0,0 +1,12 @@
+package com.fr.gc.beans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class PersonneBean implements Serializable {
+
+}
