e9827e8fe8b9489ed9d62b81fddbf98c9564b558
Habiba Abderrahim <heubu@LAPTOP-5FN1DG8J.home>
2020-08-10 12:59:31 +0100

    save method

--- src/main/java/com/fr/gc/beans/PersonneBean.java
+++ src/main/java/com/fr/gc/beans/PersonneBean.java
@@ -6,12 +6,29 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
 import com.fr.gc.entities.Personne;
+import com.fr.gc.services.PersonneService;
+import com.fr.gc.services.impl.PersonneServiceImpl;
 
 @ManagedBean
 @ViewScoped
 public class PersonneBean implements Serializable {
 
 	private Personne personne = new Personne();
+	private PersonneService personneService = new PersonneServiceImpl();
+	
+	
+	public void ajouter() {
+		
+		try {
+			personneService.save(personne);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 
 	public Personne getPersonne() {
 		return personne;
@@ -20,6 +37,5 @@ public class PersonneBean implements Serializable {
 	public void setPersonne(Personne personne) {
 		this.personne = personne;
 	}
-	
-	
+
 }
